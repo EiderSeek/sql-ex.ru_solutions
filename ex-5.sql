61ae75cbb21d10a5c5048bd2d54e0dd023f4edb6
EiderSeek <75343100+EiderSeek@users.noreply.github.com>
2021-10-19 21:12:57 +0200

    Create ex-5.sql

---
+++ ex-5.sql
@@ -0,0 +1,8 @@
+/*Exercise: 5
+Find the model number, speed and hard drive capacity of PCs cheaper than $600 having a 12x or a 24x CD drive.
+*/
+
+--Solution:
+SELECT model, speed, hd
+FROM PC
+WHERE price <600 AND (cd = '12x' OR cd = '24x');
