a6b26d5f8249a6e129ce6d86b22703ed56fc7427
EiderSeek <75343100+EiderSeek@users.noreply.github.com>
2021-10-19 21:15:59 +0200

    Create ex-6.sql

---
+++ ex-6.sql
@@ -0,0 +1,9 @@
+/*Exercise: 6
+For each maker producing laptops with a hard drive capacity of 10 Gb or higher, find the speed of such laptops. Result set: maker, speed.
+*/
+
+--Solution:
+SELECT DISTINCT maker, speed 
+FROM Laptop
+LEFT JOIN Product ON Product.model=Laptop.model
+WHERE hd>=10;
