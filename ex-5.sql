1d539be5740b5df4c23864c047f22c875e7804bf
EiderSeek <75343100+EiderSeek@users.noreply.github.com>
2021-10-19 21:15:37 +0200

    Update ex-5.sql

--- ex-5.sql
+++ ex-5.sql
@@ -5,4 +5,4 @@ Find the model number, speed and hard drive capacity of PCs cheaper than $600 ha
 --Solution:
 SELECT model, speed, hd
 FROM PC
-WHERE price <600 AND (cd = '12x' OR cd = '24x');
+WHERE price<600 AND (cd='12x' OR cd='24x');
