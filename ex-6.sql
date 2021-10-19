4b7daa2fe572f768e76a26b6a16b9400e3928a57
EiderSeek <75343100+EiderSeek@users.noreply.github.com>
2021-10-19 21:18:43 +0200

    Update ex-6.sql

--- ex-6.sql
+++ ex-6.sql
@@ -1,5 +1,5 @@
 /*Exercise: 6
-For each maker producing laptops with a hard drive capacity of 10 Gb or higher, find the speed of such laptops. Result set: maker, speed.
+For each maker producing laptops with a hard drive capacity of 10 Gb or higher, find the speed of such laptops.
 */
 
 --Solution:
