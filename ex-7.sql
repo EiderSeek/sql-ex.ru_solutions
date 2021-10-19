045df5e4ec825ad5eb9fe0eec1e4962106560763
EiderSeek <75343100+EiderSeek@users.noreply.github.com>
2021-10-19 21:18:18 +0200

    Create ex-7.sql

---
+++ ex-7.sql
@@ -0,0 +1,20 @@
+/*Exercise: 7
+Get the models and prices for all commercially available products (of any type) produced by maker B.
+*/
+
+--Solution:
+SELECT DISTINCT PC.model, PC.price
+FROM PC
+INNER JOIN Product ON Product.model=PC.model
+WHERE Product.maker='B'
+UNION
+SELECT DISTINCT Laptop.model, Laptop.price
+FROM Laptop
+INNER JOIN Product ON Product.model=Laptop.model
+WHERE Product.maker='B'
+UNION
+SELECT DISTINCT Printer.model, Printer.price
+FROM Printer
+INNER JOIN Product ON Product.model=Printer.model
+WHERE Product.maker='B';
+
