572e1044fb5849b3cb3b909e16f21b2639072bf7
EiderSeek <75343100+EiderSeek@users.noreply.github.com>
2021-10-19 21:25:28 +0200

    Create ex-10.sql

---
+++ ex-10.sql
@@ -0,0 +1,10 @@
+/*
+Exercise: 10
+Find the printer models having the highest price.
+*/
+
+--Solution:
+SELECT DISTINCT model, price 
+FROM Printer
+WHERE price=(SELECT MAX(price)
+            FROM Printer)
