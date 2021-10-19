4c42f1ef5798e486df3a0096412666309677197a
EiderSeek <75343100+EiderSeek@users.noreply.github.com>
2021-10-19 21:02:12 +0200

    Update ex-1.sql

--- ex-1.sql
+++ ex-1.sql
@@ -1,8 +1,8 @@
-Exercise: 1
+/*Exercise: 1
 Find the model number, speed and hard drive capacity for all the PCs with prices below $500.
-Result set: model, speed, hd.
+Result set: model, speed, hd.*/
 
-Solution:
+--Solution:
 SELECT PC.model, PC.speed, PC.hd
 FROM PC
 WHERE PC.price <500
