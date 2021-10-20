14c710df258afc015d43816a217169244f5c8bde
EiderSeek <75343100+EiderSeek@users.noreply.github.com>
2021-10-20 21:13:20 +0200

    Update ex-26.sql

--- ex-26.sql
+++ ex-26.sql
@@ -7,15 +7,15 @@ Find out the average price of PCs and laptops produced by maker A.
 SELECT AVG(T.price)
 FROM
     (
-      SELECT price
-      FROM   PC
-      JOIN   Product P
-      ON     PC.model=P.model
-      WHERE  P.maker='A'
-      UNION ALL
-      SELECT price
-      FROM   Laptop L
-      JOIN   Product P
-      ON     L.model=P.model
-      WHERE  P.maker='A'
+    SELECT price
+    FROM   PC
+    JOIN   Product P
+    ON     PC.model=P.model
+    WHERE  P.maker='A'
+    UNION ALL
+    SELECT price
+    FROM   Laptop L
+    JOIN   Product P
+    ON     L.model=P.model
+    WHERE  P.maker='A'
     ) T;
