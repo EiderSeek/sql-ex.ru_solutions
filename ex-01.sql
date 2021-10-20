ba412b4ca9c3afc0c7f3f862f3d7ab5a51accbd3
EiderSeek <75343100+EiderSeek@users.noreply.github.com>
2021-10-20 20:26:15 +0200

    Update ex-01.sql

--- ex-01.sql
+++ ex-01.sql
@@ -4,6 +4,8 @@ Find the model number, speed and hard drive capacity for all the PCs with prices
 */
 
 --Solution:
-SELECT PC.model, PC.speed, PC.hd
-FROM PC
-WHERE PC.price<500;
+SELECT  PC.model
+       ,PC.speed
+       ,PC.hd
+FROM    PC
+WHERE   PC.price<500;
