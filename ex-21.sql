3ceac6445de8f758d6d9e07a3b48438950d68ecf
EiderSeek <75343100+EiderSeek@users.noreply.github.com>
2021-10-20 20:24:43 +0200

    Update ex-21.sql

--- ex-21.sql
+++ ex-21.sql
@@ -4,9 +4,9 @@ Find out the maximum PC price for each maker having models in the PC table.
 */
 
 --Solution:
-SELECT p.maker
-      ,MAX(c.price) max_price
-FROM  Product p
-INNER JOIN PC 
-ON (PC.model=p.model)
-GROUP BY p.maker;
+SELECT      p.maker
+            ,MAX(price) max_price
+FROM        Product p
+JOIN        PC 
+ON          PC.model=p.model
+GROUP BY    p.maker;
