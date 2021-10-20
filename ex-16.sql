b6b5b93171965bdd3b75d651b32fcd6f627dd0f2
EiderSeek <75343100+EiderSeek@users.noreply.github.com>
2021-10-20 20:44:38 +0200

    Update ex-16.sql

--- ex-16.sql
+++ ex-16.sql
@@ -5,12 +5,12 @@ Get pairs of PC models with identical speeds and the same RAM capacity. Each res
 
 --Solution:
 SELECT DISTINCT 
-     a.model
-    ,b.model
-    ,a.speed
-    ,a.ram
-FROM PC AS a
-    ,PC AS b
+      a.model
+     ,b.model
+     ,a.speed
+     ,a.ram
+FROM  PC AS a
+     ,PC AS b
 WHERE a.ram=b.ram 
-    AND a.speed=b.speed 
-    AND a.model>b.model;
+  AND a.speed=b.speed 
+  AND a.model>b.model;
