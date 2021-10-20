c13dd575178a2d53a6c4c2da4151209ad53b1c66
EiderSeek <75343100+EiderSeek@users.noreply.github.com>
2021-10-20 20:39:25 +0200

    Update ex-10.sql

--- ex-10.sql
+++ ex-10.sql
@@ -5,11 +5,11 @@ Find the printer models having the highest price.
 
 --Solution:
 SELECT DISTINCT 
-         model
-        ,price 
-FROM     Printer
-WHERE    price=
-            (
-            SELECT MAX(price)
-            FROM   Printer
-            );      
+       model
+      ,price 
+FROM   Printer
+WHERE  price=
+         (
+          SELECT MAX(price)
+          FROM   Printer
+         );      
