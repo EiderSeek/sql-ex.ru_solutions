787e8f42d89fff7bc2634f3f963c5efc6d9e0d74
EiderSeek <75343100+EiderSeek@users.noreply.github.com>
2021-10-20 20:27:53 +0200

    Update ex-03.sql

--- ex-03.sql
+++ ex-03.sql
@@ -4,6 +4,8 @@ Find the model number, RAM and screen size of the laptops with prices over $1000
 */
 
 --Solution:
-SELECT model, ram, screen
-FROM Laptop
-WHERE price>1000;
+SELECT model
+      ,ram
+      ,screen
+FROM   Laptop
+WHERE  price>1000;
