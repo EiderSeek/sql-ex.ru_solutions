bf1ad0db7c20f3ec23c4a363413ef0de044f934e
EiderSeek <75343100+EiderSeek@users.noreply.github.com>
2021-10-20 20:36:49 +0200

    Update ex-09.sql

--- ex-09.sql
+++ ex-09.sql
@@ -4,7 +4,9 @@ Find the makers of PCs with a processor speed of 450 MHz or more.
 */
 
 --Solution:
-SELECT DISTINCT maker
-FROM Product
-INNER JOIN PC ON Pc.model=Product.model
+SELECT DISTINCT 
+      maker
+FROM  Product P
+JOIN  PC 
+ON    PC.model=P.model
 WHERE PC.speed>=450;
