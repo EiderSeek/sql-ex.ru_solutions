19205d9830d6ca4003051863d1a45bb022415424
EiderSeek <75343100+EiderSeek@users.noreply.github.com>
2021-10-20 20:25:38 +0200

    Update ex-20.sql

--- ex-20.sql
+++ ex-20.sql
@@ -4,9 +4,9 @@ Find the makers producing at least three distinct models of PCs.
 */
 
 --Solution:
-SELECT p.maker
-      ,COUNT(p.model) No_PC_Models
-FROM   Product p
-WHERE  p.type='PC'
-GROUP BY p.maker
-HAVING COUNT(p.model)>=3;
+SELECT      p.maker
+           ,COUNT(p.model) No_PC_Models
+FROM        Product p
+WHERE       p.type='PC'
+GROUP BY    p.maker
+HAVING      COUNT(p.model)>=3;
