37c16203d85d41327f408e2f091bb7173f5d3a82
EiderSeek <75343100+EiderSeek@users.noreply.github.com>
2021-10-20 19:23:10 +0200

    Create ex-17.sql

---
+++ ex-17.sql
@@ -0,0 +1,18 @@
+/*
+Exercise: 17
+Get the laptop models that have a speed smaller than the speed of any PC.
+*/
+
+--Solution:
+SELECT DISTINCT 
+       a.type
+      ,l.model
+      ,l.speed
+FROM   Product AS a
+      ,Laptop AS l
+WHERE l.speed<ALL
+  (
+  SELECT p.speed
+  FROM PC AS p
+  )
+  AND a.type='Laptop'
