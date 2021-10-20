b106ea9735e1a1ecf875421946951a3923bcb227
EiderSeek <75343100+EiderSeek@users.noreply.github.com>
2021-10-20 20:20:58 +0200

    Create ex-25.sql

---
+++ ex-25.sql
@@ -0,0 +1,30 @@
+/*
+Exercise: 25
+Find the printer makers also producing PCs with the lowest RAM capacity and the highest processor speed of all PCs having the lowest RAM capacity.
+*/
+
+--Solution:
+SELECT p.maker
+FROM   product p
+WHERE  type='Printer'
+INTERSECT
+SELECT p.maker
+FROM   product p
+JOIN   PC
+ON     p.model=PC.model
+WHERE  speed= 
+  (
+    SELECT MAX(speed)
+    FROM   PC
+    WHERE  ram=
+    (
+      SELECT MIN(ram)
+      FROM   PC
+    )
+  )
+  AND ram=
+  (
+  SELECT MIN(ram)
+  FROM   PC
+  )
+
