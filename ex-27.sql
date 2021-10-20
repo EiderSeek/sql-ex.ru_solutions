8fbb1ca89e5ef7e9cb2a2a6d2cd8f94b8f710907
EiderSeek <75343100+EiderSeek@users.noreply.github.com>
2021-10-20 22:55:08 +0200

    Create ex-27.sql

---
+++ ex-27.sql
@@ -0,0 +1,21 @@
+/*
+Exercise: 27
+Find out the average hard disk drive capacity of PCs produced by makers who also manufacture printers.
+*/
+
+--Solution:
+SELECT PM.maker
+      ,AVG(PM.hd)
+FROM   (
+       SELECT P.maker, PC.hd
+       FROM Product P
+       JOIN PC
+       ON P.model=PC.model
+       WHERE P.maker IN
+             (
+             SELECT maker
+             FROM Product
+             WHERE type='Printer'
+             )
+       ) PM
+GROUP BY PM.maker;
