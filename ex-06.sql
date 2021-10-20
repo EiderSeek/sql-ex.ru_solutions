e3425773b360f7424ad2cb38214baddd05728ca7
EiderSeek <75343100+EiderSeek@users.noreply.github.com>
2021-10-20 20:32:18 +0200

    Update ex-06.sql

--- ex-06.sql
+++ ex-06.sql
@@ -7,7 +7,7 @@ For each maker producing laptops with a hard drive capacity of 10 Gb or higher, 
 SELECT DISTINCT 
             maker
            ,speed 
-FROM        Laptop
-LEFT JOIN   Product 
-ON          Product.model=Laptop.model
+FROM        Laptop L
+LEFT JOIN   Product P
+ON          P.model=L.model
 WHERE       hd>=10;
