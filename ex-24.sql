2cc5e1bd30ead5ad7e4bc791c9ef1526fd2afc22
EiderSeek <75343100+EiderSeek@users.noreply.github.com>
2021-10-20 20:07:45 +0200

    Create ex-24.sql

---
+++ ex-24.sql
@@ -0,0 +1,43 @@
+/*
+Exercise: 24
+List the models of any type having the highest price of all products present in the database.
+*/
+
+--Solution:
+WITH  Total_CTE 
+AS 
+(
+  SELECT model
+        ,price 
+  FROM   PC
+  WHERE  price=
+    (
+      SELECT MAX(price)
+      FROM   PC
+    )
+  UNION
+  SELECT model
+        ,price 
+  FROM   Laptop
+  WHERE  price=
+    (
+      SELECT MAX(price)
+      FROM   Laptop
+    )
+  UNION
+  SELECT model
+        ,price
+  FROM   Printer  
+  WHERE  price=
+    (
+      SELECT MAX(price)
+      FROM   Printer
+    )
+)
+SELECT model
+FROM   Total_CTE
+WHERE  price=
+(
+  SELECT MAX(price)
+  FROM   Total_CTE
+)
