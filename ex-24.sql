239399b015d2b0ad4e4f2ba8e1415d570c10f05b
EiderSeek <75343100+EiderSeek@users.noreply.github.com>
2021-10-20 23:01:40 +0200

    Update ex-24.sql

--- ex-24.sql
+++ ex-24.sql
@@ -7,37 +7,37 @@ List the models of any type having the highest price of all products present in 
 WITH  Total_CTE 
 AS 
 (
-  SELECT model
-        ,price 
-  FROM   PC
-  WHERE  price=
-         (
-         SELECT MAX(price)
-         FROM   PC
-         )
-  UNION
-  SELECT model
-        ,price 
-  FROM   Laptop
-  WHERE  price=
-         (
-         SELECT MAX(price)
-         FROM   Laptop
-         )
-  UNION
-  SELECT model
-        ,price
-  FROM   Printer  
-  WHERE  price=
-         (
-         SELECT MAX(price)
-         FROM   Printer
-         )
+SELECT model
+      ,price 
+FROM   PC
+WHERE  price=
+              (
+              SELECT MAX(price)
+              FROM   PC
+              )
+UNION
+SELECT model
+      ,price 
+FROM   Laptop
+WHERE  price=
+              (
+              SELECT MAX(price)
+              FROM   Laptop
+              )
+UNION
+SELECT model
+      ,price
+FROM   Printer  
+WHERE  price=
+              (
+              SELECT MAX(price)
+              FROM   Printer
+              )
 )
 SELECT model
 FROM   Total_CTE
 WHERE  price=
-       (
-       SELECT MAX(price)
-       FROM   Total_CTE
-       );
+              (
+              SELECT MAX(price)
+              FROM   Total_CTE
+              );
