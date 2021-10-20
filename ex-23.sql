44c2bed0faf84caa81371b278dcd9bd5637ace32
EiderSeek <75343100+EiderSeek@users.noreply.github.com>
2021-10-20 19:44:53 +0200

    Create ex-23.sql

---
+++ ex-23.sql
@@ -0,0 +1,17 @@
+/*
+Exercise: 23
+Get the makers producing both PCs having a speed of 750 MHz or higher and laptops with a speed of 750 MHz or higher.
+*/
+
+--Solution:
+SELECT P.maker
+FROM   Product P
+INNER JOIN PC
+ON P.model=PC.model
+WHERE PC.speed>=750
+INTERSECT
+SELECT P.maker
+FROM   Product P
+INNER JOIN Laptop L
+ON P.model=L.model
+WHERE L.speed>=750;
